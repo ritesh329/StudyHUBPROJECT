85d0a3ef420282831eff9cc828f010279910e216
ritesh-kumar-maurya <ritesh.maurya@deboxglobal.com>
2026-03-22 10:09:05 +0530

    updated code

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -4,8 +4,12 @@ import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen relative">
-      
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat relative"
+      style={{
+        backgroundImage: "url('/images/backgroundimg.jpeg')",
+      }}
+    >
       {/* Overlay */}
       <div className="absolute inset-0 bg-white/30 dark:bg-black/60"></div>
 
